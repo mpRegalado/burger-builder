9624e51381d5eb9118795256224e41026fc14550
Manuel Gomez <mpr.gomezc@gmail.com>
2021-03-03 12:30:01 +0100

    Made the modal panel appear when clicking the order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,9 +22,13 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
 
   updatePurchaseState = (ingredients) => {
     let purchasable = false;
@@ -74,7 +78,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
@@ -83,7 +87,8 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
-          purchasable={this.state.purchasable} />
+          purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}/>
       </Aux>
     );
   }
