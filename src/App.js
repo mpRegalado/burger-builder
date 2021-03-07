e57fc23555362b2f3fd5b82d7b7a9a111520ae81
Manuel Gomez <mpr.gomezc@gmail.com>
2021-03-07 18:02:44 +0100

    Installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
+
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
+      <BrowserRouter>
         <Layout>
           <BurgerBuilder />
         </Layout>
+      </BrowserRouter>
       </div>
     );
   }
