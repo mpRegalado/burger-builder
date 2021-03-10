e324794b358838a3321cab8f33a86b2987ad5913
Manuel Gomez <mpr.gomezc@gmail.com>
2021-03-10 17:43:39 +0100

    Moved Routing to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, {Component} from 'react';
 
 import Layout from './containers/Layout/Layout'
 import { BrowserRouter } from 'react-router-dom';
+import ErrorPrompt from './components/ErrorPrompt/ErrorPrompt';
+import { Route, Switch } from 'react-router'
+import Checkout from './containers/Checkout/Checkout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import '@elastic/eui/dist/eui_theme_light.css';
+import PastOrders from './containers/PastOrders/PastOrders';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Layout />
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/pastOrders" component={PastOrders}/>
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route><ErrorPrompt errorTitle="404 nothing here"/></Route>
+          </Switch>
+        </Layout>
       </BrowserRouter>
     );
   }
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,46 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Header from '../../components/Header/Header'
-import BurgerBuilder from '../BurgerBuilder/BurgerBuilder'
-import Checkout from '../Checkout/Checkout'
-import { Route, Switch } from 'react-router'
 import { 
   EuiPage,
   EuiPageBody,
   EuiPageContent
  } from '@elastic/eui'
 
-class Layout extends Component{
-
-  state = {
-    showSideDrawer: false
-  }
-
-  sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer: false});
-  }
-  sideDrawerOpenHandler = () => {
-    this.setState({showSideDrawer: true});
-  }
-
-  render (){
-    return (
-      <div style={{display:"flex", minHeight:"100vh"}}>
-      <EuiPage direction="column">
-        <EuiPageBody restrictWidth={true} direction="column" paddingSize="l">
-          <Header />
-          <EuiPageContent
-            verticalPosition="center"
-            horizontalPosition="center">
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-          </EuiPageContent>
-        </EuiPageBody>
-      </EuiPage>
-      </div>
-    )
-  };
+const Layout = props => {
+  return (
+    <div style={{display:"flex", minHeight:"100vh"}}>
+    <EuiPage direction="column">
+      <EuiPageBody restrictWidth={true} direction="column" paddingSize="l">
+        <Header />
+        <EuiPageContent
+          verticalPosition="center"
+          horizontalPosition="center">
+            {props.children}
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
+    </div>
+  )
 };
 
 export default Layout;
